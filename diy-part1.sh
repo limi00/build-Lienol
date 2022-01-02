9e467c09bf52fb8b992e1771b440ca7c292a1676
limi00 <75186809+limi00@users.noreply.github.com>
2022-01-02 21:33:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,8 +21,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '$a src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
 sed -i 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 sed -i 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 # # Define My Package
 # git clone https://github.com/limi00/luci-app-adguardhome.git package/diy/luci-app-adguardhome
