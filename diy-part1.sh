2adec64613170f096949df1754d0f5d110b180d1
limi00 <75186809+limi00@users.noreply.github.com>
2022-01-03 12:56:18 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -26,11 +26,11 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
-echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld.git' >> feeds.conf.default
 
 # # Define My Package
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
+# git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/diy/luci-app-usb3disable
