48f581617ed7e5c3975d8ceb8b982d36492dfd85
limi00 <75186809+limi00@users.noreply.github.com>
2022-01-03 10:43:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,21 +17,23 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-echo 'src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
 # sed -i'$a src-git project https://github.com/project-openwrt/openwrt' feeds.conf.default
 # sed -i'$a src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
-# echo ' src-git small8 https://github.com/kenzok8/small-package' feeds.conf.default
-# echo ' src-git small https://github.com/kenzok8/small' feeds.conf.default
+# echo ' src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
+# echo ' src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-echo 'src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
-echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
-# echo 'src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
+echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
 
 # # Define My Package
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
 git clone https://github.com/rufengsuixing/luci-app-usb3disable.git package/diy/luci-app-usb3disable
+git clone  netspeedtest  https://github.com/sirpdboy/netspeedtest package/diy/luci-app-netspeedtest
+git clone  autotimeset  https://github.com/sirpdboy/luci-app-autotimeset package/diy/luci-app-autotimeset
 # git clone https://github.com/tty228/luci-app-serverchan.git package/mine/luci-app-serverchan
 
 rm -rf ./package/lean/luci-theme-argon && git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon  #新的argon主题
