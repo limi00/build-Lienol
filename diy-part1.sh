a6f385afe8095c769038dd784a581171c5d934a4
limi00 <75186809+limi00@users.noreply.github.com>
2022-01-05 19:24:20 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -10,7 +10,6 @@
 
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 替换更新adguardhome
 # rm -rf package/diy/luci-app-adguardhome && git clone https://github.com/limi00/luci-app-adguardhome.git package/diy/luci-app-adguardhome
@@ -19,8 +18,8 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i'$a src-git project https://github.com/project-openwrt/openwrt' feeds.conf.default
 # sed -i'$a src-git openwrt https://github.com/openwrt/packages' feeds.conf.default
-echo ' src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
-echo ' src-git small https://github.com/kenzok8/small' >> feeds.conf.default
+# echo ' src-git small8 https://github.com/kenzok8/small-package' >> feeds.conf.default
+# echo ' src-git small https://github.com/kenzok8/small' >> feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
