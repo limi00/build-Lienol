7f7a6446174c834a1b0bd06e2f0add0f39b11e0a
limi00 <75186809+limi00@users.noreply.github.com>
2022-05-25 20:56:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,13 +12,18 @@
 #
 
 # Uncomment a feed source
-sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # 替换更新adguardhome
 # rm -rf package/diy/luci-app-adguardhome && git clone https://github.com/limi00/luci-app-adguardhome.git package/diy/luci-app-adguardhome
 
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+
+# Lienol/openwrt-package
+#sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' >>feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
