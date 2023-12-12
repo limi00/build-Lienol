d464b22c9ce2ff0880fda55bb328886ffd5eda5c
limi00 <75186809+limi00@users.noreply.github.com>
2023-12-12 20:21:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,16 +14,23 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 替换更新adguardhome
-# rm -rf package/diy/luci-app-adguardhome && git clone https://github.com/limi00/luci-app-adguardhome.git package/diy/luci-app-adguardhome
+# 克隆源码
+# adguardhome
+git clone -b main --depth 1 https://github.com/XiaoBinin/luci-app-adguardhome.git
+# passwall2
+git clone -b main --depth 1 https://github.com/xiaorouji/openwrt-passwall2.git
+# mosdns
+git clone -b v5 --depth 1 https://github.com/sbwml/luci-app-mosdns.git
+# v2ray-geodata
+git clone https://github.com/sbwml/v2ray-geodata
+# lucky
+git clone -b main --depth 1 https://github.com/sirpdboy/luci-app-lucky.git
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
-
-# Lienol/openwrt-package
 #sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' >>feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' >>feeds.conf.default
@@ -37,7 +44,7 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 # # Define My Package
-git clone https://github.com/Ausaci/luci-app-nat6-helper.git package/diy/luci-app-nat6-helper.git
+# git clone https://github.com/Ausaci/luci-app-nat6-helper.git package/diy/luci-app-nat6-helper.git
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/diy/luci-app-adguardhome
 # git clone https://github.com/xiaorouji/openwrt-passwall.git package/diy/luci-app-passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/diy/luci-app-passwall2
