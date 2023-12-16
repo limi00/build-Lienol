7a460c1457a972c2cf516cd5a3c55756f01451e5
limi00 <75186809+limi00@users.noreply.github.com>
2023-12-16 15:56:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,10 +28,10 @@
 
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' feeds.conf.default
-#sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' >>feeds.conf.default
+sed -i '$a src-git lienol https://github.com/Lancenas/lienol-openwrt-package.git' >>feeds.conf.default
 # sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 # sed -i '$a src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 # sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
